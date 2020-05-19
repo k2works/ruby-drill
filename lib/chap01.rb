73da36c33579da08be9aaccb53dcd01922381e3a
k2works <kakimomokuri@gmail.com>
2020-05-19 13:14:55 +0900

    chore: セットアップ

---
+++ lib/chap01.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Adds the given numbers togther
+#
+# >> add(2,2)
+# => 5
+def add(a, b)
+  a + b
+end
--- test/chap01_test.rb
+++ test/chap01_test.rb
@@ -3,6 +3,7 @@
 require 'minitest/autorun'
 require 'minitest/reporters'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+require_relative '../lib/chap01'
 
 class MiniTestAdd < Minitest::Test
   def setup; end
@@ -10,12 +11,4 @@ class MiniTestAdd < Minitest::Test
   def test_add
     assert_equal 5, add(2, 2)
   end
-end
-
-# Adds the given numbers togther
-#
-# >> add(2,2)
-# => 5
-def add(a, b)
-  a + b
 end
